a747a7288093ef96fff1062e3619c84182f59280
Odd Gunnar Aspaas <oddgas@stud.ntnu.no>
2020-03-07 11:50:16 +0100

    Feature/oga/user profile

--- user/forms.py
+++ user/forms.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 from projects.models import ProjectCategory
 from django.contrib.auth.forms import UserCreationForm
 
+
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-    company = forms.CharField(max_length=30, required=False, help_text= 'Here you can add your company.')
-    
-    email = forms.EmailField(max_length=254, help_text='Inform a valid email address.')
-    email_confirmation = forms.EmailField(max_length=254, help_text='Enter the same email as before, for verification.')
-    
+    company = forms.CharField(
+        max_length=30, required=False, help_text='Here you can add your company.')
+
+    email = forms.EmailField(
+        max_length=254, help_text='Inform a valid email address.')
+    email_confirmation = forms.EmailField(
+        max_length=254, help_text='Enter the same email as before, for verification.')
+
     phone_number = forms.CharField(max_length=50)
 
     country = forms.CharField(max_length=50)
@@ -18,9 +22,21 @@ class SignUpForm(UserCreationForm):
     city = forms.CharField(max_length=50)
     postal_code = forms.CharField(max_length=50)
     street_address = forms.CharField(max_length=50)
-
-    categories = forms.ModelMultipleChoiceField(queryset=ProjectCategory.objects.all(), help_text='Hold down "Control", or "Command" on a Mac, to select more than one.')
+    description = forms.CharField(max_length=2000)
+    categories = forms.ModelMultipleChoiceField(queryset=ProjectCategory.objects.all(
+    ), help_text='Hold down "Control", or "Command" on a Mac, to select more than one.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'categories', 'company' , 'email', 'email_confirmation', 'password1', 'password2', 'phone_number', 'country', 'state', 'city', 'postal_code', 'street_address')
+        fields = ('username', 'first_name', 'last_name', 'categories', 'company', 'email', 'email_confirmation',
+                  'password1', 'password2', 'phone_number', 'country', 'state', 'city', 'postal_code', 'street_address', 'description')
+
+# COPY PASTA
+
+
+class UpdateProfile(forms.ModelForm):
+    description = forms.CharField(max_length=2000)
+
+    class Meta:
+        model = User
+        fields = ('description',)
